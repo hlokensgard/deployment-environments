d99fa32e497ada269aaebc3491e13e1bc040045f
Havard LOKENSGARD <havard.lokensgard@soprasteria.com>
2023-11-21 20:53:54 +0100

    Moved everything to main.tf to test

--- Environments/SubscriptionVending/main.tf
+++ Environments/SubscriptionVending/main.tf
@@ -3,3 +3,18 @@ resource "azurerm_resource_group" "this" {
     location = "westeurope"
 }
 
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
--- Environments/SubscriptionVending/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "azurerm" {
-  features {}
-
-  skip_provider_registration = true
-}
